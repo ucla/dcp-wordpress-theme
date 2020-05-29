9f8b34a85ae4fa6e73fe990b8c8c460ee9f1b8c3
Scott Vosburgh <svosburgh@stratcomm.ucla.edu>
2020-05-29 14:36:36 -0700

    - add accordion and flipping arrow

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -1,32 +1,10 @@
 $(document).ready(function (){
 
-  //add arrow to accrodion title
-  $('.accordion__title').append('<img class="accordion__title-arrow" src="/img/arrow-down.svg" alt="arrow">');
-
-  $('img.accordion__title-arrow[src$=".svg"]').each(function () {
-    let $img = jQuery(this);
-    let imgURL = $img.attr('src');
-    let attributes = $img.prop('attributes');
-
-    $.get(imgURL, function (data) {
-      // Get the SVG tag, ignore the rest
-      let $svg = jQuery(data).find('svg');
-      // Remove any invalid XML tags
-      $svg = $svg.removeAttr('xmlns:a');
-      // Loop through IMG attributes and apply on SVG
-      $.each(attributes, function () {
-        $svg.attr(this.name, this.value);
-      });
-      // Replace IMG with SVG
-      $img.replaceWith($svg);
-    }, 'xml');
-  });
-
   //This is what happens when you click the title on mobile or desktop.
   $('.accordion-title').click(function (){
     $(this).next('.accordion-content').toggleClass('show-me');
     //$(this).parent('.accordion').toggleClass('show-me');
-    //$(this).find('.accordion__title-arrow').toggleClass('flip');
+    $(this).children('img').toggleClass('flip');
     //$(this).toggleClass('active');
 
     // if ($(this).next('.accordion__content').find('a').attr('tabindex') === -1) {
--- templates/faq-loop.php
+++ templates/faq-loop.php
@@ -2,7 +2,7 @@
 <?php //FAQ Loop
 $args = array(
     'post_type' => 'faq',
-    'tag' => 'campus-operations',
+    'tag__in' => 3,
     'post_status' => 'publish',
     'posts_per_page' => 0,
 );
@@ -12,7 +12,7 @@ $loop = new WP_Query( $args );
 while ( $loop->have_posts() ) : $loop->the_post(); ?>
 
   <div class="accordion">
-    <p class="accordion-title"><?php print the_title(); ?><img src="/wp-content/themes/ucla-sc/images/icons/arrow-down-defaultblue.svg"</p>
+    <p class="accordion-title"><?php print the_title(); ?><img class"accordion-title--arrow" src="/wp-content/themes/ucla-sc/images/icons/arrow-down-defaultblue.svg"</p>
     <div class="accordion-content mt-32"><?php the_content(); ?></div>
   </div>
 
