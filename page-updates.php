8301d6482323cd5edc5a4edb8525784528b5ea00
Scott Vosburgh <svosburgh@stratcomm.ucla.edu>
2020-06-12 15:47:44 -0700

    updates intro

--- page-updates.php
+++ page-updates.php
@@ -17,6 +17,16 @@ $thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ) );
         <div class="col span_12_of_12">
           <div class="breadcrumb"><?php get_breadcrumb(); ?></div>
           <h1 class="entry-title"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
+          <?php if ( get_post_custom_values( 'intro' ) !== NULL )  { ?>
+            <p class="intro"><?php
+            $key_values = get_post_custom_values( 'intro' );
+
+            foreach ( $key_values as $key => $value ) {
+                echo $value;
+            }
+
+            ?></p>
+          <?php } ?>
         </div>
       </div>
     </header>
