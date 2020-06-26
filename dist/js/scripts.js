39c3385986fa6188b20fc9fd9d05d625150ae5a4
Scott Vosburgh <svosburgh@stratcomm.ucla.edu>
2020-06-26 10:59:18 -0700

    - add hover states to accordions - adjust primary nav for better viewing last menu item on mobile - add link to ucla logo in header

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -3,8 +3,6 @@ $(document).ready(function (){
   //This is what happens when you click the title on mobile or desktop.
   $('.accordion-title').click(function (){
     $(this).next('.accordion-content').toggleClass('show-me');
-    //$(this).parent('.accordion').toggleClass('show-me');
-    $(this).children('img').toggleClass('flip');
     $(this).toggleClass('active');
 
     if ($('.accordion-title').hasClass('active')) {
