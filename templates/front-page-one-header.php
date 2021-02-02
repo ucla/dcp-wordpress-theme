dfb7748eaa40d89bb1ca192443a70e8eead5f9ed
Scott Vosburgh <svosburgh@stratcomm.ucla.edu>
2021-02-02 13:22:09 -0800

    add custom template add front page one and two templates adjust breadcrumb spacing add some Gutenberg specific adjustments for branding

--- templates/front-page-one-header.php
+++ templates/front-page-one-header.php
@@ -5,24 +5,25 @@
       <img src="<?php if ( has_post_thumbnail() ) { echo $image; } ?>" alt="">
     </div>
 
-
     <div class="front-page-hero__info">
+      <div class="front-page-hero__info--wrap">
 
-      <h1 class="front-page-hero__title"><?php the_title(); ?></h1>
+        <h1 class="front-page-hero__title"><?php the_title(); ?></h1>
 
-      <?php //edit_post_link(); ?>
+        <?php //edit_post_link(); ?>
 
-      <p class="front-page-hero__intro">
-        <?php  $key_values = get_post_custom_values( 'intro' );
+        <p class="front-page-hero__intro">
+          <?php  $key_values = get_post_custom_values( 'intro' );
 
-          if (is_array($key_values) || is_object($key_values)) {
-            foreach ( $key_values as $key => $value ) {
-              echo $value;
+            if (is_array($key_values) || is_object($key_values)) {
+              foreach ( $key_values as $key => $value ) {
+                echo $value;
+              }
             }
-        } ?>
-      </p>
+          ?>
+         </p>
 
+      </div>
     </div>
-
   </div>
 </header>
