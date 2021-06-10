2da1bcd5be8c36191df55340c4850a95c59adce8
Scott Vosburgh <svosburgh@stratcomm.ucla.edu>
2021-06-10 15:23:29 -0700

    comp lib update, new svgs, header update

--- page.php
+++ page.php
@@ -7,9 +7,9 @@
 
         <div class="ucla campus">
           <div class="col span_12_of_12">
-            <div class="breadcrumb mb-24"><?php get_breadcrumb(); ?> / <?php the_title(); ?></div>
-            <h1 class="mb-24 mb-md-32"><?php the_title(); ?></h1>
-            <p><?php edit_post_link(); ?></p>
+            <div class="breadcrumb"><?php get_breadcrumb(); ?> / <?php the_title(); ?></div>
+            <h1 class="mb-24"><?php the_title(); ?></h1>
+            <?php edit_post_link(); ?>
           </div>
         </div>
 
