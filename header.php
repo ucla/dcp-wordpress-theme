d5d038d4c2bf236123c95e7f246d9d90f8170d7d
Alex Tou <atou@Alexs-MacBook-Pro.local>
2022-01-04 10:45:23 -0800

    add missing GTM script

--- header.php
+++ header.php
@@ -2,6 +2,20 @@
 <html <?php language_attributes(); ?>>
 
   <head>
+    <?php
+      if ( myprefix_get_theme_option('gtm_input') !== NULL ) {
+        $gtm_tag = myprefix_get_theme_option('gtm_input');
+        ?>
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); 
+        })(window,document,'script','dataLayer','<?php echo $gtm_tag;?>');</script>
+        <!-- End Google Tag Manager -->
+        <?php
+      }
+    ?>
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <meta name="viewport" content="width=device-width" />
     <?php wp_head(); ?>
