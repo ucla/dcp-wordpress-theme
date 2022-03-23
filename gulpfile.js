9b2a2ca8ad82e0d4a5bbd49264e8b22e7f3df07e
Scott Gruber <scott@scottgruber.me>
2022-03-23 15:59:19 -0700

    update gulp script to generate min and source maps

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,18 @@
 'use strict';
 
-const { src, series, dest, parallel, watch, task } = require('gulp');
-const cleanCSS = require('gulp-clean-css');
-const sass = require('gulp-sass');
-const sass = require('gulp-sass')
-const gulpStylelint = require('gulp-stylelint');
+const {
+  src,
+  dest,
+  watch,
+  series
+} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
+const del = require('del');
 const eslint = require('gulp-eslint');
-
-sass.compiler = require('sass')
+const gulpStylelint = require('gulp-stylelint');
+const minify = require('gulp-minify');
+const sourcemaps = require('gulp-sourcemaps');
 
 // For debugging, make sure gulp is installed
 function defaultTask(cb) {
@@ -18,63 +22,97 @@ function defaultTask(cb) {
 // Compile the theme styles
 function themeStyles() {
   return src('assets/scss/**/*.scss')
-    .pipe(sass.sync({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(sourcemaps.init())
+    .pipe(sass.sync({
+      outputStyle: 'expanded'
+    }).on('error', sass.logError))
     .pipe(concat('global.css'))
+    .pipe(sourcemaps.write(''))
+    .pipe(dest('dist/css'));
+}
+
+function compressThemeStyles() {
+  return src('assets/scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass.sync({
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(concat('global.min.css'))
+    .pipe(sourcemaps.write(''))
     .pipe(dest('dist/css'));
 }
 
 // Lint Theme styles
-function lintSassWatch() {
+function lintSass() {
   return src('assets/scss/**/*.scss')
     .pipe(gulpStylelint({
-      reporters: [
-        {formatter: 'string', console: true}
-      ]
+      reporters: [{
+        formatter: 'string',
+        console: true
+      }]
     }));
 }
 
 // Watch Theme style
 function watchStyles(done) {
-  watch('assets/scss/**/*.scss', series(themeStyles, lintSassWatch)),
+  watch('assets/scss/**/*.scss',
+    series(themeStyles, lintSass)),
   done();
 }
 
-function lintJavascript() {
-   return src('assets/js/**/*.js')
-     .pipe(eslint())
-     .pipe(eslint.format())
-     // To have the process exit with an error code (1) on
-     // lint error, return the stream and pipe to failAfterError last.
-     //.pipe(eslint.failAfterError());
+function lintJs() {
+  return src('assets/js/**/*.js')
+    .pipe(eslint())
+    .pipe(eslint.format());
 }
 
 // Bundle the js added in the theme.
-function themeConcatJs() {
+function themeJs() {
   return src('assets/js/**/*.js')
+    .pipe(sourcemaps.init())
     .pipe(concat('scripts.js'))
+    .pipe(minify({
+      ext: {
+        src: '.js',
+        min: '.min.js'
+      },
+    }))
+    .pipe(sourcemaps.write(''))
     .pipe(dest('dist/js'));
 }
 
 // watch the theme scritps while linting
-function watchJavascript(done) {
-  watch('assets/js/*.js', series(themeConcatJs, lintJavascript));
+function watchJs(done) {
+  watch('assets/js/*.js',
+    series(themeJs, lintJs));
   done();
 }
 
-exports.default = defaultTask
+function cleanJsStyles() {
+  return del([
+    'dist/js/*',
+    'dist/css/*'
+  ]);
+}
+
+exports.default = defaultTask;
 
 
 exports.build = series(
+  cleanJsStyles,
+  compressThemeStyles,
   themeStyles,
-  themeConcatJs
+  themeJs
 );
 
 exports.production = series(
+  cleanJsStyles,
   themeStyles,
-  themeConcatJs
+  compressThemeStyles,
+  themeJs
 );
 
 exports.watch = series(
   watchStyles,
-  watchJavascript
+  watchJs
 );
