25c4ec388316ba163f1ba6df563ddcc3a4a06bcf
Jacob Linder <jacob.linder.jl@gmail.com>
2022-05-15 21:13:13 -0700

    Basic carousel

--- templates/front-page-carousel-header.php
+++ templates/front-page-carousel-header.php
@@ -1,6 +1,19 @@
 <header class="header">
   <div class="front-page-hero">
-
+    <?php
+      $post_meta_value = get_post_meta( $post->ID, 'carousel-title', true );
+      if ($post_meta_value !== null) {
+        echo '<img class="title-image" src="./wp-content/themes/ucla-wp/images/Molecule.svg" />';
+        echo '<h1 class="title-text">';
+        $title_words = explode(" ", $post_meta_value);
+        foreach ($title_words as $title_word) {
+          echo '<span class="title-word">';
+          echo $title_word;
+          echo '</span>';
+        }
+        echo '</h1>';
+      }
+    ?>
     <script src='https://cdn.jsdelivr.net/npm/@splidejs/splide@4.0.1/dist/js/splide.min.js'></script>
     <div id="splide-front-page-carousel" class="splide">
       <div class="splide__slider">
@@ -11,15 +24,17 @@
               $images = &get_children( array (
                 'post_mime_type' => 'image'
               ));
-              if ( empty($images) ) {
-                // no attachments here
-              } else {
+              if ( !empty($images) ) {
                 foreach ( $images as $attachment_id => $attachment ) {
-                  $image_attributes = wp_get_attachment_image_src($attachment_id);
+                  $image_attributes = wp_get_attachment_image_src($attachment_id);                  
                   if ( $image_attributes ) {
-                    echo '<li className="splide__slide">';
-                    echo '<img src="'; echo $image_attributes[0]; echo '" width="1000" height="700">';
-                    echo '</li>';
+                    $img_url = $image_attributes[0];
+                    $img_title = get_the_title($attachment_id);
+                    if(strpos($img_title, "__carousel") == false)
+                      continue;
+                    echo '<li class="splide__slide"><div class="splide__slide__container">';
+                    echo '<img src="' . $img_url . '" width="auto" height="auto">';                    
+                    echo '</div></li>';
                   }
                 }
               }
@@ -31,10 +46,15 @@
     <script>
       document.addEventListener("DOMContentLoaded", function(event) {
         new Splide("#splide-front-page-carousel", {
-          type: 'loop',
-          width: '100%',
-          height: '70vh',
-          cover: true,
+            type: 'loop',
+            width: '100%',
+            height: '70vh',
+            cover: true,
+            classes: {
+                arrow: 'splide__arrow carousel-arrows',
+                next: 'splide__arrow--next carousel-arrow-right',
+                pagination: 'splide__pagination carousel-pagination-internal'
+            }
         }).mount();
       });
     </script>
