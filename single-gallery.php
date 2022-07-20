a0c39c385b6a133e4e2eb60204e2372b6c10eed8
Katherine Cardoza <kcardoza@stratcomm.ucla.edu>
2022-07-20 15:41:14 -0700

    related content slider work internal gallery page

--- single-gallery.php
+++ single-gallery.php
@@ -26,12 +26,38 @@
                 <?php edit_post_link(); ?>
             </div>
         </div>
-        <div class="ucla campus related-content">
-            <h2>Related Content</h2>
-            <?php $recent_posts = wp_get_recent_posts(array('post_type'=>'gallery'));
-            foreach( $recent_posts as $recent ){
-                echo '<li><a href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" >' .   $recent["post_title"].'</a> </li> ';
-            } ?>
+        <div class="ucla campus related-content pb-96">
+            <div class="col span_12_of_12">
+                <h2>Related Content</h2>
+                <div id="example-card" class="splide">
+                    <div class="splide__slider">
+                        <!-- relative -->
+                        <div class="splide__track">
+                            <ul class="splide__list">
+                            <?php $recent_posts = wp_get_recent_posts(array('post_type'=>'gallery', 'numberposts' => '5'));
+                                foreach( $recent_posts as $recent ){
+                                    echo '<li class="splide__slide"> <article class="story__secondary-card has-background-lightest-grey-2"><a class="story__secondary-card-link" href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" > <h3 class="story__secondary-card-title"><span>' .   $recent["post_title"].'</span></h3></a></article></li> ';
+                                } ?>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </main>
 <?php get_footer(); ?>
+
+<script>
+    new Splide("#example-card", {
+        perPage: 3,
+        rewind: true,
+        breakpoints: {
+            768: {
+                perPage: 2,
+            },
+            600: {
+                perPage: 1,
+            },
+        }
+    }).mount();
+</script>
