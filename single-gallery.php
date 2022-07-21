ccdc79ca627993987399ad92ada624219809272b
Katherine Cardoza <kcardoza@stratcomm.ucla.edu>
2022-07-21 11:13:01 -0700

    related posts component gallery cpt

--- single-gallery.php
+++ single-gallery.php
@@ -35,9 +35,20 @@
                             <!-- relative -->
                             <div class="splide__track">
                                 <ul class="splide__list">
-                                <?php $recent_posts = wp_get_recent_posts(array('post_type'=>'gallery', 'numberposts' => '5'));
+                                <?php 
+                                    $recent_posts = wp_get_recent_posts(array('post_type'=>'gallery', 'numberposts' => '5'));
+
                                     foreach( $recent_posts as $recent ){
-                                        echo '<li class="splide__slide"> <article class="basic-card-grey"><a class="story__secondary-card-link" href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" > <div class="basic-card__info-wrapper"><h1 class="basic-card__title"><span>' .   $recent["post_title"].'</span></h1></div></a></article></li> ';
+                                        $thumb_id = get_post_thumbnail_id( $recent["ID"] );
+                                        if ( '' != $thumb_id ) {
+                                            $thumb_url  = wp_get_attachment_image_src( $thumb_id, 'full', true );
+                                            $image      = $thumb_url[0];
+                                        }
+                                        else {
+                                            $image = '/wp-content/themes/ucla-wp/images/overlay.jpg';
+                                        }
+
+                                        echo '<li class="splide__slide"> <article class="basic-card"><img class="basic-card__image" src="' . $image . '" alt="' . $recent["post_title"] . '"><a class="story__secondary-card-link" href="' . get_permalink($recent["ID"]) . '" title="Look ' . esc_attr($recent["post_title"]).'" > <div class="basic-card__info-wrapper"><h1 class="basic-card__title"><span>' .   $recent["post_title"] .'</span></h1>' . '<p class="basic-card__description">' . get_the_excerpt($recent["ID"]) . '</p></div></a></article></li> ';
                                     } ?>
                                 </ul>
                             </div>
@@ -51,6 +62,14 @@
 <script>
     new Splide("#related-card", {
         perPage: 3,
-        rewind: true
+        rewind: true, 
+        breakpoints: {
+            1024: {
+                perPage: 2,
+            },
+            600: {
+                perPage: 1,
+            },
+        }
     }).mount();
 </script>
