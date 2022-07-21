190ae4f62017a9a60ab480453cbe2ad1baa06e8d
Katherine Cardoza <kcardoza@stratcomm.ucla.edu>
2022-07-20 17:28:34 -0700

    work on related content component

--- single-gallery.php
+++ single-gallery.php
@@ -26,19 +26,21 @@
                 <?php edit_post_link(); ?>
             </div>
         </div>
-        <div class="ucla campus related-content pb-96">
-            <div class="col span_12_of_12">
-                <h2>Related Content</h2>
-                <div id="example-card" class="splide">
-                    <div class="splide__slider">
-                        <!-- relative -->
-                        <div class="splide__track">
-                            <ul class="splide__list">
-                            <?php $recent_posts = wp_get_recent_posts(array('post_type'=>'gallery', 'numberposts' => '5'));
-                                foreach( $recent_posts as $recent ){
-                                    echo '<li class="splide__slide"> <article class="story__secondary-card has-background-lightest-grey-2"><a class="story__secondary-card-link" href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" > <h3 class="story__secondary-card-title"><span>' .   $recent["post_title"].'</span></h3></a></article></li> ';
-                                } ?>
-                            </ul>
+        <div class="section-wrapper">
+            <div class="ucla campus related-content pt-80 pb-80">
+                <div class="col span_12_of_12">
+                    <h2>Related Content</h2>
+                    <div id="related-card" class="splide">
+                        <div class="splide__slider">
+                            <!-- relative -->
+                            <div class="splide__track">
+                                <ul class="splide__list">
+                                <?php $recent_posts = wp_get_recent_posts(array('post_type'=>'gallery', 'numberposts' => '5'));
+                                    foreach( $recent_posts as $recent ){
+                                        echo '<li class="splide__slide"> <article class="basic-card-grey"><a class="story__secondary-card-link" href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" > <div class="basic-card__info-wrapper"><h1 class="basic-card__title"><span>' .   $recent["post_title"].'</span></h1></div></a></article></li> ';
+                                    } ?>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -46,18 +48,9 @@
         </div>
     </main>
 <?php get_footer(); ?>
-
 <script>
-    new Splide("#example-card", {
+    new Splide("#related-card", {
         perPage: 3,
-        rewind: true,
-        breakpoints: {
-            768: {
-                perPage: 2,
-            },
-            600: {
-                perPage: 1,
-            },
-        }
+        rewind: true
     }).mount();
 </script>
