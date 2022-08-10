ac2cfe67e94053af40cbfe07c7e8f05b0cf609c3
Scott Gruber <scott@scottgruber.me>
2022-08-10 15:21:16 -0700

    format document

--- options.php
+++ options.php
@@ -7,185 +7,182 @@
 
 // Exit if accessed directly
 if (!defined('ABSPATH')) {
-	exit;
+    exit;
 }
 
 // Start Class
 if (!class_exists('UCLA_Theme_Options')) {
+    class UCLA_Theme_Options
+    {
+        /**
+         * Start things up
+         *
+         * @since 1.0.0
+         */
+        public function __construct()
+        {
 
-	class UCLA_Theme_Options
-	{
+            // We only need to register the admin panel on the back-end
+            if (is_admin()) {
+                add_action('admin_menu', array('UCLA_Theme_Options', 'add_admin_menu'));
+                add_action('admin_init', array('UCLA_Theme_Options', 'register_settings'));
+            }
+        }
 
-		/**
-		 * Start things up
-		 *
-		 * @since 1.0.0
-		 */
-		public function __construct()
-		{
+        /**
+         * Returns all theme options
+         *
+         * @since 1.0.0
+         */
+        public static function get_theme_options()
+        {
+            return get_option('theme_options');
+        }
 
-			// We only need to register the admin panel on the back-end
-			if (is_admin()) {
-				add_action('admin_menu', array('UCLA_Theme_Options', 'add_admin_menu'));
-				add_action('admin_init', array('UCLA_Theme_Options', 'register_settings'));
-			}
-		}
+        /**
+         * Returns single theme option
+         *
+         */
+        public static function get_theme_option($id)
+        {
+            $options = self::get_theme_options();
+            if (isset($options[$id])) {
+                return $options[$id];
+            }
+        }
 
-		/**
-		 * Returns all theme options
-		 *
-		 * @since 1.0.0
-		 */
-		public static function get_theme_options()
-		{
-			return get_option('theme_options');
-		}
+        /**
+         * Add sub menu page
+         *
+         * @since 1.0.0
+         */
+        public static function add_admin_menu()
+        {
+            add_menu_page(
+                esc_html__('Theme Settings', 'text-domain'),
+                esc_html__('Theme Settings', 'text-domain'),
+                'manage_options',
+                'theme-settings',
+                array('UCLA_Theme_Options', 'create_admin_page')
+            );
+        }
 
-		/**
-		 * Returns single theme option
-		 *
-		 */
-		public static function get_theme_option($id)
-		{
-			$options = self::get_theme_options();
-			if (isset($options[$id])) {
-				return $options[$id];
-			}
-		}
+        /**
+         * Register a setting and its sanitization callback.
+         *
+         * We are only registering 1 setting so we can store all options in a single option as
+         * an array. You could, however, register a new setting for each option
+         *
+         * @since 1.0.0
+         */
+        public static function register_settings()
+        {
+            register_setting('theme_options', 'theme_options', array('UCLA_Theme_Options', 'sanitize'));
+        }
 
-		/**
-		 * Add sub menu page
-		 *
-		 * @since 1.0.0
-		 */
-		public static function add_admin_menu()
-		{
-			add_menu_page(
-				esc_html__('Theme Settings', 'text-domain'),
-				esc_html__('Theme Settings', 'text-domain'),
-				'manage_options',
-				'theme-settings',
-				array('UCLA_Theme_Options', 'create_admin_page')
-			);
-		}
+        /**
+         * Sanitization callback
+         *
+         * @since 1.0.0
+         */
+        public static function sanitize($options)
+        {
 
-		/**
-		 * Register a setting and its sanitization callback.
-		 *
-		 * We are only registering 1 setting so we can store all options in a single option as
-		 * an array. You could, however, register a new setting for each option
-		 *
-		 * @since 1.0.0
-		 */
-		public static function register_settings()
-		{
-			register_setting('theme_options', 'theme_options', array('UCLA_Theme_Options', 'sanitize'));
-		}
+            // If we have options lets sanitize them
+            if ($options) {
 
-		/**
-		 * Sanitization callback
-		 *
-		 * @since 1.0.0
-		 */
-		public static function sanitize($options)
-		{
+                // Address Heading/Unit Name
+                if (!empty($options['address_heading'])) {
+                    $options['address_heading'] = sanitize_textarea_field($options['address_heading']);
+                } else {
+                    unset($options['address_heading']); // Remove from options if empty
+                }
 
-			// If we have options lets sanitize them
-			if ($options) {
+                // Address Line One
+                if (!empty($options['address_input_one'])) {
+                    $options['address_input_one'] = sanitize_textarea_field($options['address_input_one']);
+                } else {
+                    unset($options['address_input_one']); // Remove from options if empty
+                }
 
-				// Address Heading/Unit Name	
-				if (!empty($options['address_heading'])) {
-					$options['address_heading'] = sanitize_textarea_field($options['address_heading']);
-			} else {
-					unset($options['address_heading']); // Remove from options if empty
-			}
+                // Address Line Two
+                if (!empty($options['address_input_two'])) {
+                    $options['address_input_two'] = sanitize_textarea_field($options['address_input_two']);
+                } else {
+                    unset($options['address_input_two']); // Remove from options if empty
+                }
 
-				// Address Line One
-				if (!empty($options['address_input_one'])) {
-					$options['address_input_one'] = sanitize_textarea_field($options['address_input_one']);
-				} else {
-					unset($options['address_input_one']); // Remove from options if empty
-				}
+                // Phone line
+                if (!empty($options['phone_input'])) {
+                    $options['phone_input'] = sanitize_textarea_field($options['phone_input']);
+                } else {
+                    unset($options['phone_input']); // Remove from options if empty
+                }
 
-				// Address Line Two
-				if (!empty($options['address_input_two'])) {
-					$options['address_input_two'] = sanitize_textarea_field($options['address_input_two']);
-				} else {
-					unset($options['address_input_two']); // Remove from options if empty
-				}
+                // Email line
+                if (!empty($options['email_input'])) {
+                    $options['email_input'] = sanitize_textarea_field($options['email_input']);
+                } else {
+                    unset($options['email_input']); // Remove from options if empty
+                }
 
-				// Phone line
-				if (!empty($options['phone_input'])) {
-					$options['phone_input'] = sanitize_textarea_field($options['phone_input']);
-				} else {
-					unset($options['phone_input']); // Remove from options if empty
-				}
+                // GTM Tag
+                if (!empty($options['gtm_input'])) {
+                    $options['gtm_input'] = sanitize_textarea_field($options['gtm_input']);
+                } else {
+                    unset($options['gtm_input']); // Remove from options if empty
+                }
 
-				// Email line
-				if (!empty($options['email_input'])) {
-					$options['email_input'] = sanitize_textarea_field($options['email_input']);
-				} else {
-					unset($options['email_input']); // Remove from options if empty
-				}
+                // Social links
+                if (!empty($options['facebook_input'])) {
+                    $options['facebook_input'] = sanitize_textarea_field($options['facebook_input']);
+                } else {
+                    unset($options['facebook_input']); // Remove from options if empty
+                }
+                if (!empty($options['twitter_input'])) {
+                    $options['twitter_input'] = sanitize_textarea_field($options['twitter_input']);
+                } else {
+                    unset($options['twitter_input']); // Remove from options if empty
+                }
+                if (!empty($options['instagram_input'])) {
+                    $options['instagram_input'] = sanitize_textarea_field($options['instagram_input']);
+                } else {
+                    unset($options['instagram_input']); // Remove from options if empty
+                }
+                if (!empty($options['snapchat_input'])) {
+                    $options['snapchat_input'] = sanitize_textarea_field($options['snapchat_input']);
+                } else {
+                    unset($options['snapchat_input']); // Remove from options if empty
+                }
+                if (!empty($options['linkedin_input'])) {
+                    $options['linkedin_input'] = sanitize_textarea_field($options['linkedin_input']);
+                } else {
+                    unset($options['linkedin_input']); // Remove from options if empty
+                }
+                if (!empty($options['youtube_input'])) {
+                    $options['youtube_input'] = sanitize_textarea_field($options['youtube_input']);
+                } else {
+                    unset($options['youtube_input']); // Remove from options if empty
+                }
+                if (!empty($options['tiktok_input'])) {
+                    $options['tiktok_input'] = sanitize_textarea_field($options['tiktok_input']);
+                } else {
+                    unset($options['tiktok_input']); // Remove from options if empty
+                }
+            }
 
-				// GTM Tag
-				if (!empty($options['gtm_input'])) {
-					$options['gtm_input'] = sanitize_textarea_field($options['gtm_input']);
-				} else {
-					unset($options['gtm_input']); // Remove from options if empty
-				}
-
-				// Social links
-				if (!empty($options['facebook_input'])) {
-					$options['facebook_input'] = sanitize_textarea_field($options['facebook_input']);
-				} else {
-					unset($options['facebook_input']); // Remove from options if empty
-				}
-				if (!empty($options['twitter_input'])) {
-					$options['twitter_input'] = sanitize_textarea_field($options['twitter_input']);
-				} else {
-					unset($options['twitter_input']); // Remove from options if empty
-				}
-				if (!empty($options['instagram_input'])) {
-					$options['instagram_input'] = sanitize_textarea_field($options['instagram_input']);
-				} else {
-					unset($options['instagram_input']); // Remove from options if empty
-				}
-				if (!empty($options['snapchat_input'])) {
-					$options['snapchat_input'] = sanitize_textarea_field($options['snapchat_input']);
-				} else {
-					unset($options['snapchat_input']); // Remove from options if empty
-				}
-				if (!empty($options['linkedin_input'])) {
-					$options['linkedin_input'] = sanitize_textarea_field($options['linkedin_input']);
-				} else {
-					unset($options['linkedin_input']); // Remove from options if empty
-				}
-				if (!empty($options['youtube_input'])) {
-					$options['youtube_input'] = sanitize_textarea_field($options['youtube_input']);
-				} else {
-					unset($options['youtube_input']); // Remove from options if empty
-				}
-				if (!empty($options['tiktok_input'])) {
-					$options['tiktok_input'] = sanitize_textarea_field($options['tiktok_input']);
-				} else {
-					unset($options['tiktok_input']); // Remove from options if empty
-				}
-
-			}
-
-			// Return sanitized options
-			return $options;
-		}
+            // Return sanitized options
+            return $options;
+        }
 
 
-		/**
-		 * Settings page output
-		 *
-		 * @since 1.0.0
-		 */
-		public static function create_admin_page()
-		{ ?>
+        /**
+         * Settings page output
+         *
+         * @since 1.0.0
+         */
+        public static function create_admin_page()
+        { ?>
 
 			<div class="wrap">
 
@@ -196,15 +193,15 @@ if (!class_exists('UCLA_Theme_Options')) {
 					<?php settings_fields('theme_options'); ?>
 
 					<table class="form-table UCLA-custom-admin-login-table">
-						<?php // Text input example 
-						?>
+						<?php // Text input example
+                        ?>
 						<tr valign="top">
-              <th scope="row"><?php esc_html_e('Footer Name', 'text-domain'); ?></th>
-              <td>
+                            <th scope="row"><?php esc_html_e('Footer Name', 'text-domain'); ?></th>
+                            <td>
 								<?php $address_heading = self::get_theme_option('address_heading'); ?>
 								<input type="text" name="theme_options[address_heading]" placeholder="Add name of academic unit" value="<?php echo esc_attr($address_heading); ?>" />
 							</td>
-            </tr>
+                        </tr>
 						<tr valign="top">
 							<th scope="row"><?php esc_html_e('Footer Address', 'text-domain'); ?></th>
 							<td>
@@ -296,12 +293,12 @@ if (!class_exists('UCLA_Theme_Options')) {
 
 			</div><!-- .wrap -->
 <?php }
-	}
+        }
 }
 new UCLA_Theme_Options();
 
 // Helper function to use in your theme to return a theme option value
 function myprefix_get_theme_option($id = '')
 {
-	return UCLA_Theme_Options::get_theme_option($id);
+    return UCLA_Theme_Options::get_theme_option($id);
 }
